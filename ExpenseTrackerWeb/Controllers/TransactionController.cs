df960de9b5a2c232accac2071587c8911d6a1955
rftibule <rftibule@gmail.com>
2023-02-03 01:14:24 +0800

    Added Transactions

---
+++ ExpenseTrackerWeb/Controllers/TransactionController.cs
@@ -0,0 +1,24 @@
+﻿using ExpenseTrackerWeb.Data;
+using ExpenseTrackerWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace ExpenseTrackerWeb.Controllers
+{
+    public class TransactionController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public TransactionController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IActionResult Index()
+        {
+            var _lst = _dbContext.Transactions.Include(i => i.Category).ToList();
+
+            return View(_lst);
+        }
+    }
+}
